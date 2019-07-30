63c9bbefd5b78072424eed47e7a274f6535e698d
qfx@yonyou.com <qfx@yonyou.com>
2019-07-30 10:28:30 +0800

    Initial commit

---
+++ microservice-provider/src/main/java/com/lzj/springcloud/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.lzj.springcloud.service.impl;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.lzj.springcloud.dao.UserDao;
+import com.lzj.springcloud.entity.User;
+import com.lzj.springcloud.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public boolean addUser(User user) {
+        boolean flag;
+        flag = userDao.addUser(user);
+        return flag;
+    }
+
+    @Override
+    public User getUser(int id) {
+        User user = userDao.getUser(id);
+        return user;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        List<User> users = userDao.getUsers();
+        return users;
+    }
+
+}
