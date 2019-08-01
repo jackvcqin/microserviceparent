3cdeaf4da082a1798f8e1865e9a31b6fe4f58e15
qfx@yonyou.com <qfx@yonyou.com>
2019-08-01 09:39:03 +0800

    init eureka

--- microservice-provider/src/main/java/com/lzj/springcloud/ProviderApplication.java
+++ microservice-provider/src/main/java/com/lzj/springcloud/ProviderApplication.java
@@ -1,8 +1,10 @@
 package com.lzj.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient   //启动后自动注册金eureka服务中
 public class ProviderApplication {
 
     public static void main(String[] args) {
